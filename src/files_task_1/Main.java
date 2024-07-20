5745b3d2b771b2e6e94b145aefa4f9e8ee681459
AlKham <greenowlkitty@gmail.com>
2024-07-20 23:17:36 +0500

    Edit task_1

--- src/files_task_1/Main.java
+++ src/files_task_1/Main.java
@@ -3,103 +3,96 @@ package files_task_1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder();
+    public static final String GAMES = "D:/Нетология/Games";
 
-        File games = new File("D:\\Нетология\\Games");
+    public static StringBuilder sb = new StringBuilder();
 
-        // Cоздание директорий src, res, savegames, temp в папке Games
+    public static List<File> createDir() {
 
-        File src = new File(games, "src");
-        if (src.mkdir())
-            sb.append("Создан каталог: " + src).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + src).append(System.lineSeparator());
+        //Создание списка для хранения директорий и путей к ним
+        List<String> dir = new ArrayList<>();
 
-        File res = new File(games, "res");
-        if (res.mkdir())
-            sb.append("Создан каталог: " + res).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + res).append(System.lineSeparator());
+        // Добавление в список директорий src, res, savegames, temp в папке Games
+        dir.add(GAMES + "/src");
+        dir.add(GAMES + "/res");
+        dir.add(GAMES + "/savegames");
+        dir.add(GAMES + "/temp");
 
-        File savegames = new File(games, "savegames");
-        if (savegames.mkdir())
-            sb.append("Создан каталог: " + savegames).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + savegames).append(System.lineSeparator());
+        // Добавление в список директорий main, test в каталоге src
+        dir.add(GAMES + "/src/main");
+        dir.add(GAMES + "/src/test");
 
-        File temp = new File(games, "temp");
-        if (temp.mkdir())
-            sb.append("Создан каталог: " + temp).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + temp).append(System.lineSeparator());
+        // Добавление в список директорий drawables, vectors, icons в каталог res
+        dir.add(GAMES + "/res/drawables");
+        dir.add(GAMES + "/res/vectors");
+        dir.add(GAMES + "/res/icons");
 
-        // Создание директорий main, test в каталоге src
-
-        File main = new File(src, "main");
-        if (main.mkdir())
-            sb.append("Создан каталог: " + main).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + main).append(System.lineSeparator());
-
-        File test = new File(src, "test");
-        if (test.mkdir())
-            sb.append("Создан каталог: " + test).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + test).append(System.lineSeparator());
-
-        // Создание файлов Main.java, Utils.java в подкаталоге main
-
-        File mainFile = new File(main, "Main.java");
-        try {
-            if (mainFile.createNewFile())
-                sb.append("Создан файл: " + mainFile).append(System.lineSeparator());
-        } catch (IOException ex) {
-            sb.append("Ошибка создания файла " + mainFile + " : " + ex.getMessage())
-                    .append(System.lineSeparator());
+        //Преобразование списка String в File
+        List<File> directories = new ArrayList<>();
+        for (int i = 0; i < dir.size(); i++) {
+            directories.add(new File(dir.get(i)));
         }
 
-        File utilsFile = new File(main, "Utils.java");
-        try {
-            if (utilsFile.createNewFile())
-                sb.append("Создан файл: " + utilsFile).append(System.lineSeparator());
-        } catch (IOException ex) {
-            sb.append("Ошибка создания файла " + utilsFile + " : " + ex.getMessage())
-                    .append(System.lineSeparator());
+        //Создание директорий:
+        for (File directory : directories) {
+            if (directory.mkdir())
+                sb.append("Создан каталог: " + directory).append(System.lineSeparator());
+            else sb.append("Ошибка создания каталога: " + directory).append(System.lineSeparator());
+        }
+        return directories;
+    }
+
+    public static List<File> createFiles() {
+
+        //Создание списка для хранения директорий и путей к ним
+        List<String> filesList = new ArrayList<>();
+
+        // Добавление в список файлов Main.java, Utils.java в подкаталоге main
+        filesList.add(GAMES + "/src/main/Main.java");
+        filesList.add(GAMES + "/src/main/Utils.java");
+
+        // Добавление в список файла temp.txt в директории temp
+        filesList.add(GAMES + "/temp/temp.txt");
+
+        //Преобразование списка String в File
+        List<File> files = new ArrayList<>();
+        for (int i = 0; i < filesList.size(); i++) {
+            files.add(new File(filesList.get(i)));
         }
 
-        // Создание директорий drawables, vectors, icons в каталог res
-
-        File drawables = new File(res, "drawables");
-        if (drawables.mkdir())
-            sb.append("Создан каталог: " + drawables).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + drawables).append(System.lineSeparator());
-
-        File vectors = new File(res, "vectors");
-        if (vectors.mkdir())
-            sb.append("Создан каталог: " + vectors).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + vectors).append(System.lineSeparator());
-
-        File icons = new File(res, "icons");
-        if (icons.mkdir())
-            sb.append("Создан каталог: " + icons).append(System.lineSeparator());
-        else sb.append("Ошибка создания каталога: " + icons).append(System.lineSeparator());
-
-        // Создание файла temp.txt в директории temp
-
-        File tempFile = new File(temp, "temp.txt");
-        try {
-            if (tempFile.createNewFile())
-                sb.append("Создан файл: " + tempFile).append(System.lineSeparator());
-        } catch (IOException ex) {
-            sb.append("Ошибка создания файла " + tempFile + " : " + ex.getMessage())
-                    .append(System.lineSeparator());
+        //Создание файлов:
+        for (File file : files) {
+            try {
+                if (file.createNewFile())
+                    sb.append("Создан файл: " + file).append(System.lineSeparator());
+            } catch (IOException ex) {
+                sb.append("Ошибка создания файла " + file + " : " + ex.getMessage())
+                        .append(System.lineSeparator());
+            }
         }
+        return files;
+    }
+
+    public static void recordLog() {
+        String tempFile = GAMES + "\\temp\\temp.txt";
 
         // Запись лога в файл temp.txt
-
         try (FileWriter writer = new FileWriter(tempFile, true)) {
             writer.write(String.valueOf(sb));
             writer.flush();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        createDir();
+        createFiles();
+        recordLog();
     }
 }
